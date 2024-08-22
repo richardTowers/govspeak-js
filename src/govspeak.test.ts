035226363bf7820f8d84517fa09c2d676d5c5083
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-08-22 12:12:52 +0100

    Initial sanitizing
    NOTE: this is NOT SECURE! Needs doing properly with dompurify or
    similar.

--- src/govspeak.test.ts
+++ src/govspeak.test.ts
@@ -8,3 +8,9 @@ test('renders a simple paragraph', async () => {
 test('renders a heading', async () => {
   expect(await render('## a heading')).toEqual('<h2>a heading</h2>\n')
 })
+
+test('does not allow injections', async () => {
+  // Technically, this is valid markdown, according to the spec - https://spec.commonmark.org/0.28/#example-138
+
+  expect(await render('<script>alert("lol pwned")</script>')).toEqual('&lt;script&gt;alert("lol pwned")&lt;/script&gt;')
+})
--- src/govspeak.ts
+++ src/govspeak.ts
@@ -1,5 +1,10 @@
 import { marked } from 'marked'
 
 export async function render(input: string) {
-  return await marked(input, { async: true })
+  const dirtyHtml = await marked(input, { async: true })
+
+  // The ruby version of govspeak also sanitizes HTML after running kramdown - https://github.com/alphagov/govspeak/blob/main/lib/govspeak/html_sanitizer.rb
+  const cleanHtml = dirtyHtml.replace(/<(\/?script)/g, '&lt;$1').replace(/(script)>/g, '$1&gt;') // TODO!!! - santize this properly!!!
+
+  return cleanHtml
 }
