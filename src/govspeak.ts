9fc774451cac579c04725b399bfb0f6d03ef05bc
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-08-22 10:13:40 +0100

    Add kramed with default settings

--- src/govspeak.ts
+++ src/govspeak.ts
@@ -1,3 +1,7 @@
+import { default as k, KramedStatic } from 'kramed'
+
+const kramed = k as unknown as KramedStatic // Use TypeScript, they said. It'll be fun, they said.
+
 export function render(input: string) {
-  return input
+  return kramed(input)
 }
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { render } from './govspeak.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h2>Govspeak</h2>
-    <textarea id="input" rows=20 cols=100></textarea>
+    <textarea id="input" rows="20" cols="100"></textarea>
     <h2>HTML</h2>
     <div id="output"></div>
   </div>
 `
 
-function go() {
+function run() {
   const result = render(document.querySelector<HTMLTextAreaElement>('#input')!.value)
   document.querySelector<HTMLDivElement>('#output')!.innerText = result
 }
-document.addEventListener('keyup', go)
-go
+document.addEventListener('keyup', run)
+run
