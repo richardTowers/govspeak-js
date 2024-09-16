f0ffa6288332664698f2ec838e834554fb4e805a
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-09-16 17:37:28 +0100

    Upgrade remark-abbr, still got a type problem

--- src/govspeak.ts
+++ src/govspeak.ts
@@ -2,21 +2,24 @@ import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 import rehypeStringify from 'rehype-stringify'
 import remarkParse from 'remark-parse'
-import remarkRehype, {Options as RehypeOptions} from 'remark-rehype'
 import remarkAbbr from '@richardtowers/remark-abbr'
+import remarkRehype, {Options as RehypeOptions} from 'remark-rehype'
 import {unified} from 'unified'
 import schema from './sanitize/allowed-schema'
+
+const rehypeOptions: RehypeOptions = {
+  handlers: {
+    abbrDefinition() {
+      return undefined
+    }
+  }
+}
 
 export async function render(input: string) {
   const html = await unified()
     .use(remarkParse)
     .use(remarkAbbr)
-    .use(remarkRehype, {
-      allowDangerousHtml: true,
-      handlers: {
-        abbrDefinition: () => undefined,
-      }
-    } as RehypeOptions)
+    .use(remarkRehype, rehypeOptions)
     .use(rehypeRaw)
     .use(rehypeSanitize, schema)
     .use(rehypeStringify).process(input)
